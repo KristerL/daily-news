aaf1372ff655bf9d996850075ff04df097d2293e
Krister Looga <looga.krister@gmail.com>
2019-04-08 20:02:09 +0300

    Updates

---
+++ precache-manifest.00b8913b6614418f74336188a67c5342.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ebf984a39d47ca0c158",
+    "url": "//github.com/KristerL/daily-news/static/css/main.ef16c588.chunk.css"
+  },
+  {
+    "revision": "0ebf984a39d47ca0c158",
+    "url": "//github.com/KristerL/daily-news/static/js/main.aae22fce.chunk.js"
+  },
+  {
+    "revision": "fc5426da3ebb8f63614c",
+    "url": "//github.com/KristerL/daily-news/static/js/runtime~main.1dfdc307.js"
+  },
+  {
+    "revision": "a6ddb70975a908f85231",
+    "url": "//github.com/KristerL/daily-news/static/js/2.51f574a4.chunk.js"
+  },
+  {
+    "revision": "f3cccae6501eaeb7da5a27766ba32e46",
+    "url": "//github.com/KristerL/daily-news/index.html"
+  }
+];
